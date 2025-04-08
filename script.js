55513f10d449cd45963504c1e05ff09bb8c24bb2
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-04-08 23:22:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-//complete this code
 class Person {
-	constructor(name,age){
-		this._name=name;
-		this._age=age;
+	constructor(name, age) {
+		this._name = name;
+		this._age = age;
 	}
-	get name(){
+
+	get name() {
 		return this._name;
 	}
 
-	set age(){
+	get age() { // Add this getter
 		return this._age;
 	}
 
-	 set age(newAge) {
-    this._age = newAge;
-  }
+	set age(newAge) {
+		this._age = newAge;
+	}
 }
 
 class Student extends Person {
-	constructor(name,age){
-		super(name,age);
+	constructor(name, age) { // Corrected spelling
+		super(name, age);
 	}
-	study(){
-		console.log(`${this.name} is studying`)
+
+	study() {
+		console.log(`${this.name} is studying`);
 	}
 }
 
 class Teacher extends Person {
-	constructor(name,age){
-		super(name,age);
+	constructor(name, age) { // Corrected spelling
+		super(name, age);
 	}
-	teach(){
-		console.log(`${this.name} is teaching`)
+
+	teach() {
+		console.log(`${this.name} is teaching`);
 	}
-	
 }
 
 // Do not change the code below this line
